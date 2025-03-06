d11bdb7f08f182dcf9b7d7997e5554fc0e60160f
Kyle Wadas <wadaskyle03@gmail.com>
2025-03-06 16:35:05 -0700

    Added Player Velocity Variable

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # from pyglet.gl import *  # noqa: F403
 # from pyglet.graphics import TextureGroup
 import window
-from pyglet import image
 from pyglet.gl import *  # noqa: F403
 
 def setup_fog():
@@ -12,34 +11,34 @@ def setup_fog():
     """
     # Enable fog. Fog "blends a fog color with each rasterized pixel fragment's
     # post-texturing color."
-    glEnable(GL_FOG)
+    glEnable(GL_FOG) # noqa: F405
     # Set the fog color.
-    glFogfv(GL_FOG_COLOR, (GLfloat * 4)(0.5, 0.69, 1.0, 1))
+    glFogfv(GL_FOG_COLOR, (GLfloat * 4)(0.5, 0.69, 1.0, 1)) # noqa: F405
     # Say we have no preference between rendering speed and quality.
-    glHint(GL_FOG_HINT, GL_DONT_CARE)
+    glHint(GL_FOG_HINT, GL_DONT_CARE) # noqa: F405
     # Specify the equation used to compute the blending factor.
-    glFogi(GL_FOG_MODE, GL_LINEAR)
+    glFogi(GL_FOG_MODE, GL_LINEAR) # noqa: F405
     # How close and far away fog starts and ends. The closer the start and end,
     # the denser the fog in the fog range.
-    glFogf(GL_FOG_START, 20.0)
-    glFogf(GL_FOG_END, 60.0)
+    glFogf(GL_FOG_START, 20.0) # noqa: F405
+    glFogf(GL_FOG_END, 60.0) # noqa: F405
 
 def setup():
     """ Basic OpenGL configuration.
 w
     """
     # Set the color of "clear", i.e. the sky, in rgba.
-    glClearColor(0.5, 0.69, 1.0, 1)
+    glClearColor(0.5, 0.69, 1.0, 1) # noqa: F405
     # Enable culling (not rendering) of back-facing facets -- facets that aren't
     # visible to you.
-    glEnable(GL_CULL_FACE)
+    glEnable(GL_CULL_FACE) # noqa: F405
     # Set the texture minification/magnification function to GL_NEAREST (nearest
     # in Manhattan distance) to the specified texture coordinates. GL_NEAREST
     # "is generally faster than GL_LINEAR, but it can produce textured images
     # with sharper edges because the transition between texture elements is not
     # as smooth."
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)  # noqa: F405
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST) # noqa: F405
     setup_fog()
 
 def main():
@@ -47,7 +46,7 @@ def main():
     # Hide the mouse cursor and prevent the mouse from leaving the window.
     userWindow.set_exclusive_mouse(True)
     setup()
-    pyglet.app.run()
+    pyglet.app.run() # noqa: F405
 
 if __name__ == '__main__':
     main()
