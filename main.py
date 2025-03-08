1a0a1815540bd6fc414392cc1067dd1952924fb0
Kyle Wadas <wadaskyle03@gmail.com>
2025-03-08 14:46:34 -0700

    Moved Visual Functions to Window

--- main.py
+++ main.py
@@ -5,43 +5,11 @@
 import window
 from pyglet.gl import *  # noqa: F403
 
-def setup_fog():
-    """ Configure the OpenGL fog properties.
-    """
-    # Enable fog. Fog "blends a fog color with each rasterized pixel fragment's
-    # post-texturing color."
-    glEnable(GL_FOG) # noqa: F405
-    # Set the fog color.
-    glFogfv(GL_FOG_COLOR, (GLfloat * 4)(0.5, 0.69, 1.0, 1)) # noqa: F405
-    # Say we have no preference between rendering speed and quality.
-    glHint(GL_FOG_HINT, GL_DONT_CARE) # noqa: F405
-    # Specify the equation used to compute the blending factor.
-    glFogi(GL_FOG_MODE, GL_LINEAR) # noqa: F405
-    # How close and far away fog starts and ends. The closer the start and end,
-    # the denser the fog in the fog range.
-    glFogf(GL_FOG_START, 20.0) # noqa: F405
-    glFogf(GL_FOG_END, 60.0) # noqa: F405
-
-def setup():
-    """ Basic OpenGL configuration.
-    """
-    # Set the color of "clear", i.e. the sky, in rgba.
-    glClearColor(0.5, 0.69, 1.0, 1) # noqa: F405
-    # Enable culling (not rendering) of back-facing facets -- facets that aren't
-    # visible to you.
-    glEnable(GL_CULL_FACE) # noqa: F405
-    # Set the texture minification/magnification function to GL_NEAREST (nearest
-    # in Manhattan distance) to the specified texture coordinates. GL_NEAREST
-    # "is generally faster than GL_LINEAR, but it can produce textured images
-    # with sharper edges because the transition between texture elements is not
-    # as smooth."
-    setup_fog()
-
 def main():
     userWindow = window.Window(width=800, height=600, caption='Py Minecraft', resizable=True)
     # Hide the mouse cursor and prevent the mouse from leaving the window.
     userWindow.set_exclusive_mouse(False)
-    setup()
+    userWindow.setup()
     pyglet.app.run() # noqa: F405
 
 if __name__ == '__main__':
--- window.py
+++ window.py
@@ -14,7 +14,7 @@ class Window(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
         super(Window, self).__init__(*args, **kwargs)
         
-       # Initialize the state machine
+        # Initialize the state machine
         self.state_machine = StateMachine(GameState.MAIN_MENU)
 
         # Add states with their respective callbacks
@@ -191,6 +191,38 @@ class Window(pyglet.window.Window):
         """
         glColor3d(0, 0, 0)
         self.reticle.draw(GL_LINES)
+    
+    def setup_fog(self):
+        """ Configure the OpenGL fog properties.
+        """
+        # Enable fog. Fog "blends a fog color with each rasterized pixel fragment's
+        # post-texturing color."
+        glEnable(GL_FOG) # noqa: F405
+        # Set the fog color.
+        glFogfv(GL_FOG_COLOR, (GLfloat * 4)(0.5, 0.69, 1.0, 1)) # noqa: F405
+        # Say we have no preference between rendering speed and quality.
+        glHint(GL_FOG_HINT, GL_DONT_CARE) # noqa: F405
+        # Specify the equation used to compute the blending factor.
+        glFogi(GL_FOG_MODE, GL_LINEAR) # noqa: F405
+        # How close and far away fog starts and ends. The closer the start and end,
+        # the denser the fog in the fog range.
+        glFogf(GL_FOG_START, 20.0) # noqa: F405
+        glFogf(GL_FOG_END, 60.0) # noqa: F405
+
+    def setup(self):
+        """ Basic OpenGL configuration.
+        """
+        # Set the color of "clear", i.e. the sky, in rgba.
+        glClearColor(0.5, 0.69, 1.0, 1) # noqa: F405
+        # Enable culling (not rendering) of back-facing facets -- facets that aren't
+        # visible to you.
+        glEnable(GL_CULL_FACE) # noqa: F405
+        # Set the texture minification/magnification function to GL_NEAREST (nearest
+        # in Manhattan distance) to the specified texture coordinates. GL_NEAREST
+        # "is generally faster than GL_LINEAR, but it can produce textured images
+        # with sharper edges because the transition between texture elements is not
+        # as smooth."
+        self.setup_fog()
 
     def enter_main_menu(self):
         print("Entering Main Menu")
