e269e3230641f87f99fd409ef1805500aef2fb3f
Kyle Wadas <wadaskyle03@gmail.com>
2025-03-11 20:34:22 -0600

    Updated Terrain Generation & Improved Inventory

--- inventory.py
+++ inventory.py
@@ -3,5 +3,4 @@ import block
 class Inventory:
     def __init__(self, *args, **kwargs):
         self.hotbar = [block.PORTAL, block.STONE, block.SPRUCE_WOOD_PLANK, block.BIRCH_WOOD_PLANK, block.JUNGLE_WOOD_PLANK, block.ACACIA_WOOD_PLANK, block.DARK_OAK_WOOD_PLANK]
-        
-        self.block = self.hotbar[0]
+        self.index = 0
